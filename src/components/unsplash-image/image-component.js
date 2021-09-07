b9662eec228ddb51eefdcd6da8ef3c67938e90ae
Eugene Peter Maestrado <epmaestrado@dnamicro.com>
2021-09-08 03:05:34 +0800

    updated hooks

--- src/components/unsplash-image/image-component.js
+++ src/components/unsplash-image/image-component.js
@@ -1,72 +1,75 @@
-import React, { useEffect, Fragment } from "react";
-import { connect, useSelector } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, { useEffect, Fragment, useCallback } from 'react';
+import { connect, useSelector } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { fetchImageActionStart } from "../../redux/default-images/images.actions";
+import { fetchImageActionStart } from '../../redux/default-images/images.actions';
 
-import ImageList from "./image-list.component";
+import ImageList from './image-list.component';
 import {
-  selectDefaultImage,
-  fetchImage,
-} from "../../redux/default-images/image.selector";
+	selectDefaultImage,
+	fetchImage,
+} from '../../redux/default-images/image.selector';
 
-import { Loader, LoaderCircle } from "../loader/loader";
-import { ImageWrapper } from "./image-list-component.styles";
+import { Loader, LoaderCircle } from '../loader/loader';
+import { ImageWrapper } from './image-list-component.styles';
 
 const ImageComponent = (props) => {
-  const { fetchDefaultImage, defaultImage, isFetching } = props;
+	const { fetchDefaultImage, defaultImage, isFetching } = props;
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchDefaultImage]);
+	const fetchData = useCallback(() => {
+		fetchDefaultImage();
+	}, [fetchDefaultImage]);
 
-  useEffect(() => {
-    if (!isFetching) {
-      // alert(isFetching);
-      console.dir(defaultImage);
-    }
-  }, [isFetching, defaultImage]);
+	useEffect(() => {
+		fetchData();
+	}, [fetchDefaultImage, fetchData]);
 
-  console.log(isFetching);
+	useEffect(() => {
+		if (!isFetching) {
+			// alert(isFetching);
+			console.dir(defaultImage);
+		}
+	}, [isFetching, defaultImage]);
 
-  const fetchData = () => {
-    fetchDefaultImage();
-  };
+	console.log(isFetching);
 
-  const error = useSelector((state) => state.defaultImages.error);
-  const defaultImages = defaultImage || [];
+	const error = useSelector((state) => state.defaultImages.error);
+	const defaultImages = defaultImage || [];
 
-  return (
-    <Fragment>
-      {isFetching > 0 && <Loader />}
-      {error ? (
-        <LoaderCircle />
-      ) : (
-        <InfiniteScroll
-          dataLength={defaultImages.length} //This is important field to render the next data
-          next={fetchData}
-          hasMore={true}
-        >
-          <ImageWrapper>
-            {!defaultImages && error
-              ? ""
-              : defaultImages.map((image) => (
-                  <ImageList
-                    key={image.id}
-                    disc={image.alt_description}
-                    url={image.urls}
-                  />
-                ))}
-          </ImageWrapper>
-        </InfiniteScroll>
-      )}
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{isFetching > 0 && <Loader />}
+			{error ? (
+				<LoaderCircle />
+			) : (
+				<InfiniteScroll
+					dataLength={defaultImages.length} //This is important field to render the next data
+					next={fetchData}
+					hasMore={true}
+				>
+					<ImageWrapper>
+						{!defaultImages && error
+							? ''
+							: defaultImages.map((image, index) => (
+									<ImageList
+										key={
+											`${image.id}-${Math.random().toString()}` ??
+											`${index}-${Math.random.toString()}`
+										}
+										disc={image.alt_description}
+										url={image.urls}
+									/>
+							  ))}
+					</ImageWrapper>
+				</InfiniteScroll>
+			)}
+		</Fragment>
+	);
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchDefaultImage: () => dispatch(fetchImageActionStart()),
+	fetchDefaultImage: () => dispatch(fetchImageActionStart()),
 });
 
 // const mapStateToProps = createStructuredSelector({
@@ -74,8 +77,8 @@ const mapDispatchToProps = (dispatch) => ({
 // });
 
 const mapStateToProps = createStructuredSelector({
-  defaultImage: selectDefaultImage,
-  isFetching: fetchImage,
+	defaultImage: selectDefaultImage,
+	isFetching: fetchImage,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ImageComponent);
